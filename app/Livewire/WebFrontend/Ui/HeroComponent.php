7bddcab97eb4a88116f7b6f31cc3c74aba27a6ea
Uray Naufal <ucan05052005@gmail.com>
2024-11-19 10:50:38 +0700

    update img public vercel

--- app/Livewire/WebFrontend/Ui/HeroComponent.php
+++ app/Livewire/WebFrontend/Ui/HeroComponent.php
@@ -10,7 +10,7 @@ class HeroComponent extends Component
     public function render()
     {
         // Ambil data hero dengan status 'active'
-        $hero = Hero::where('status', 'active')->first();
+
 
         // Kirimkan data hero ke view
         return view('livewire.web-frontend.ui.hero-component', ['hero' => $hero]);
